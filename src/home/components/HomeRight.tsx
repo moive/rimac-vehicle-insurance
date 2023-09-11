fddbcca2737dbf5e3148f8d57ea97ccafdd8f5b9
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-09-11 00:43:06 -0500

    add validation form

--- src/home/components/HomeRight.tsx
+++ src/home/components/HomeRight.tsx
@@ -7,20 +7,65 @@ import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Checkbox from "@mui/material/Checkbox";
-
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+import { Controller, useForm } from "react-hook-form";
+
+type FormValues = {
+	documentNumber: string;
+	phone: string;
+	licensePlate: string;
+	acceptTerms: boolean;
+};
 
 export const HomeRight = () => {
 	const [typeDoc, setTypeDoc] = useState("dni");
+	const [isDisabled, setisDisabled] = useState(false);
 
 	const handleChange = (event: SelectChangeEvent) => {
 		setTypeDoc(event.target.value as string);
 	};
 
+	const handleChangecheck = (event: boolean) => {
+		setisDisabled(!event);
+	};
+
+	const {
+		handleSubmit,
+		register,
+		control,
+		formState: { errors },
+		// control,
+	} = useForm<FormValues>({
+		defaultValues: {
+			documentNumber: "",
+			phone: "",
+			licensePlate: "",
+			acceptTerms: true,
+		},
+	});
+
+	const onSubmit = (data: FormValues) => {
+		console.log(data);
+	};
+
+	const getMsgError = (
+		type: string | undefined,
+		name: string,
+		count: number
+	) => {
+		let msg = "";
+
+		if (type == "required") msg = `${name} es requerido`;
+		else if (type == "minLength") msg = `Número minimo es ${count}`;
+		else if (type == "maxLength") msg = `Número maximo es ${count}`;
+		return msg;
+	};
+
 	return (
 		<section className="home-right">
 			<h2>Déjanos tus datos</h2>
-			<form className="form-get-data">
+			<form className="form-get-data" onSubmit={handleSubmit(onSubmit)}>
 				<div className="row-form">
 					<FormControl fullWidth className="wid-cus-select">
 						<Select
@@ -32,33 +77,74 @@ export const HomeRight = () => {
 						</Select>
 					</FormControl>
 					<TextField
-						error={false}
 						id="documentNumber"
 						label="Nro. de doc"
-						defaultValue=""
+						type="number"
+						{...register("documentNumber", {
+							required: true,
+							pattern: /[0-9]{8}/,
+							minLength: 8,
+							maxLength: 8,
+						})}
+						error={!!errors.documentNumber}
+						helperText={getMsgError(
+							errors.documentNumber?.type,
+							"Nro. de doc",
+							8
+						)}
 					/>
 				</div>
 				<div className="row-form">
 					<TextField
-						error={false}
 						fullWidth
 						id="phone"
 						label="Celular"
-						defaultValue=""
+						type="number"
+						{...register("phone", {
+							required: true,
+							pattern: /[0-9]{9}/,
+							minLength: 9,
+							maxLength: 9,
+						})}
+						error={!!errors.phone}
+						helperText={getMsgError(
+							errors.phone?.type,
+							"Celular",
+							9
+						)}
 					/>
 				</div>
 				<div className="row-form">
 					<TextField
-						error={false}
 						fullWidth
 						id="licensePlate"
 						label="Placa"
-						defaultValue=""
+						{...register("licensePlate", {
+							required: "Placa es requerido",
+						})}
+						error={!!errors.licensePlate}
+						helperText={errors.licensePlate?.message}
 					/>
 				</div>
+
 				<div className="row-form cus-form">
 					<FormControlLabel
-						control={<Checkbox defaultChecked color="success" />}
+						control={
+							<Controller
+								control={control}
+								name="acceptTerms"
+								rules={{ required: true }}
+								render={() => (
+									<Checkbox
+										defaultChecked
+										color="success"
+										onChange={(e) =>
+											handleChangecheck(e.target.checked)
+										}
+									/>
+								)}
+							/>
+						}
 						label=""
 					/>
 					<div className="check-label">
@@ -70,7 +156,9 @@ export const HomeRight = () => {
 					</div>
 				</div>
 				<div className="group-btn">
-					<button className="btn">COTÍZALO</button>
+					<button className="btn" disabled={isDisabled}>
+						COTÍZALO
+					</button>
 				</div>
 			</form>
 		</section>
